4541eef11d789014567e1a421f4eeb38608ed534
Valeria Torres <val.torrw1@gmail.com>
2026-02-22 16:59:58 -0400

    Primer commit - estructura inicial del proyecto

---
+++ src/proyecto1/Grafo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1;
+
+/**
+ *
+ * @author valer
+ */
+//La parte mas improtante, todo lo que se tenga que hacer con el gtado es aqui
+public class Grafo {
+    
+}
---
+++ src/proyecto1/Proteina.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1;
+
+/**
+ *
+ * @author valer
+ */
+
+//La clase proteina tiene que tener el nombte de la misma y las coneziones que esta tiene con otras
+public class Proteina {
+    
+    private String nombre;
+    //private List 
+    
+}
